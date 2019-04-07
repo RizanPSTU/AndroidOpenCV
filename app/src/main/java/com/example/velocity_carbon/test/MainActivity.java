7bdf635a7c56083bb8b8a634b8e0ba79b492603f
Rafsan Uddin Beg Rizan <rafsan12@cse.pstu.ac.bd>
2019-04-07 15:02:20 +0600

    Initial commit

---
+++ app/src/main/java/com/example/velocity_carbon/test/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.velocity_carbon.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+
+        if(OpenCVLoader.initDebug()){
+            Toast.makeText(getApplicationContext(),"Hoise opencv",Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(getApplicationContext(),"Dhuuuuuuuuuu opencv",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
